9685765feb96623cc316433dd9a1b23919e63790
Evanildo <Evanildo@Evanildo.localdomain>
2018-05-25 11:49:26 -0300

    new commit

---
+++ Projeto1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body bgcolor="#00ffff">
+         Nome:<input
+type="TEXT"
+name="identificacao"
+value="insira seu nome"> </p>
+        Email:<input
+type="TEXT"
+name="identificacao"
+value="insira seu email"> </p>
+    
+    
+         senha: <input type="password" name="senha" maxlength="6"> </p>
+          <input type="submit" value="entrar"> <p>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
